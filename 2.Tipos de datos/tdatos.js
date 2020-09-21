ddb2b58846b8b8bae1e96599a88c57e73cf7c6b0
aalmohalla <59499385+aalmohalla@users.noreply.github.com>
2020-09-21 20:38:38 +0200

    Number

---
+++ 2.Tipos de datos/tdatos.js
@@ -0,0 +1,25 @@
+//"use strict"
+
+//TIPO DE DATOS NUMBER
+
+let number = 5;
+console.log(`El tipo de datos de ${number} es ${typeof(number)}`);
+
+// Existen los valores +Infinity y -Infinity
+console.log(`El resultado de 1/0 es ${1/0}`);
+
+// Una operación matemática no permitida devolvera el valor NaN: Not a Number
+console.log(` El resultado de 1/dos es ${1/"dos"}`);
+
+//NaN se propaga en una operación matemática
+
+console.log(`El esultado de 2+2+NaN es ${2+2+NaN}`);
+
+// El límite de Number es 2e53
+
+console.log(`${9007199254740991 + 100}`)
+
+
+
+// TIPO DE DATOS STRING
+
