e9d0ac5394b780d870951211e5e124c1deaaa4f5
Alfonso Almohalla González <AGA@amav.net>
2020-11-19 13:23:55 +0100

    sdgs

--- 9.Arrays/InventarioPC/js/script.js
+++ 9.Arrays/InventarioPC/js/script.js
@@ -2,7 +2,6 @@
 
  let pcList = [];//array con el inventario de objetos PC
 
-
   
 //1. FUNCIÓN newPC: debe solicitar por pantalla los datos del PC, crear un objeto con dichos datos, y
 // almacenarlo en el array pcList. el objeto tendrá las propiedades name, memory, disk, os. Todas serán string
@@ -10,9 +9,13 @@
 
 function newPC(){
    //TU CÓDIGO AQUÍ
+    let pc = {};
+    pc.name = prompt("Introduce el nombre");
+    pc.memory = prompt("Introduce la memoria");
+    pc.disk = prompt("Tamaño de disco");
+    pc.os = confirm("El sistema operativo es Windows? ");
 
-
-
+    pcList.push(pc);
 
     listClear();
     printPC(document.querySelector("#usersList"));
@@ -21,8 +24,14 @@ function newPC(){
 // Ordena los equipos por nombre, alfabeticamente de "a" a "z"
 function nameOrder(){
   //TU CÓDIGO AQUÍ
-
     
+    pcList.sort((a,b)=> {
+      if (a.name > b.name) {
+        return 1;
+      } else {
+        return -1;
+      };
+    });
     listClear();
     printPC(document.querySelector("#usersList"));    
 }
@@ -30,14 +39,21 @@ function nameOrder(){
 // Ordena los equipos por RAM, de mayor a menor
 function ramOrder() {
   //TU CÓDIGO AQUÍ
+  pcList.sort((a,b)=> a.memory - b.memory);
 
+  listClear();
+  printPC(document.querySelector("#usersList")); 
 }
 
 // Pide al usuario los GB de RAM con los que se van a ampliar todos los equipos inventariados y lo actualiza
 function ramUpdate(){
 
   //TU CÓDIGO AQUÍ
-
+let ramUpdateInput = prompt ("Introduzca la actualización de ram para todos los equipos");
+pcList=pcList.map(pc=>{
+  pc.memory = (+pc.memory + +ramUpdateInput).toString();
+  return pc;
+});
 
 
     listClear();
@@ -48,7 +64,15 @@ function ramUpdate(){
 //muestra un alert con los datos del equipo con mayor cantidad de RAM. En caso de haber varios equipos con la misma cantidad de RAM, muestra el primero por orden alfabético del nombre de equipo
 function maxRAM() {
   //TU CÓDIGO AQUÍ
-
+ 
+  let pcsortname = pcList.sort((a,b)=> {
+    if (a.name > b.name) {
+      return 1;
+    } else {
+      return -1;
+    };
+  });
+  //pcsortname.
 }
 
 // muestra un alert con la memoria RAM total de todos los equipos inventariados.
@@ -68,8 +92,8 @@ let modalWindow = M.Modal.init(document.querySelectorAll('.modal'),{dismissible:
 
 //Evento de carga de usuarios  
 document.querySelector("#newPC").addEventListener("click",newPC);
-document.querySelector("#nameOrder").addEventListener("click",nameOrder);
-document.querySelector("#ramOrder").addEventListener("click",ramOrder);
+document.querySelector("#nameSort").addEventListener("click",nameOrder);
+document.querySelector("#ramSort").addEventListener("click",ramOrder);
 document.querySelector("#ramUpdate").addEventListener("click",ramUpdate);
 document.querySelector("#maxRAM").addEventListener("click",maxRAM);
 document.querySelector("#totalRAM").addEventListener("click",totalRAM);
