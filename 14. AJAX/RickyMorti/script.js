420fd7a2d52baf9c5c6dfc721a6f8bc4c4ad74a2
aalmohalla <aalmohalla@educa.jcyl.es>
2021-01-25 23:22:06 +0100

    paginacionterminada

--- 14. AJAX/RickyMorti/script.js
+++ 14. AJAX/RickyMorti/script.js
@@ -3,16 +3,23 @@ const URL = "https://rickandmortyapi.com/api/character/"
 const NUMBERPAGE = 5;
 let currentInfo = {};
 let paginacion = document.querySelector(".row .pagination");
+let paginacionLinks = document.getElementsByClassName("pageLink");
 let query = {
   name: "",
   status: "alive",
-  currentpage: 1,
-  blockpage: 1,
-  totalpages: 1,
+  currentpage: 1, // pagina actual
+  blockpage: 1, // bloque de páginas en el que se encuentra
+  totalpages: 1, // numero de paginas
+  pagination: false, // existe paginacion en el interface?
 }
 
 let cardsView = document.querySelector(".row.fichas");
-document.querySelector("#searchButton").addEventListener("click",getData);
+document.querySelector("#searchButton").addEventListener("click",()=>{
+  paginacion.innerHTML="";
+  query.pagination = false;
+  query.page = 1;
+  getData();
+ });
 
 function getData() {
     query.name = document.querySelector("#icon_prefix").value;
@@ -24,9 +31,11 @@ function getData() {
       query.page = "1";
     } */
     fetch(URL+"?name="+ query.name + "&status=" + query.status + "&page=" + query.page)
-    .then (response=> response.json()
-    .then (datos => UIrefresh(datos)))
+    .then (response=> response.json())
+    .then (datos => UIrefresh(datos))
+    
 }
+
 
 function UIrefresh(datos) {
     console.dir(datos);
@@ -34,10 +43,11 @@ function UIrefresh(datos) {
     cardsView.innerHTML = "";
     datos.results.forEach(element => {
         cardsView.insertAdjacentHTML("beforeend", 
-    ` <div class="col s6 l4">
+    ` <div class="col s6 l4" draggable>
     <div class="card">
       <div class="card-image">
         <img src=${element.image}>
+        <i class="material-icons moveicon">zoom_out_map</i>
         <span class="card-title">${element.name}</span>
       </div>
       <div class="card-content">
@@ -52,64 +62,154 @@ function UIrefresh(datos) {
     ` )
      
     });
+    if (query.totalpages > 1 && !query.pagination) {
+      addPagination();
+    }
 
-//paginacion
-paginacion.innerHTML="";
-    if (query.totalpages > 1 && !document.querySelector(".pagination li")) {
-          paginacion.insertAdjacentHTML("beforeend",      
-      `<li id="previusPage" class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-      <li class="active"><a href="#!">1</a></li>
-      <li id="nextPage" class="disabled"><a href="#!"><i class="material-icons">chevron_right</i></a></li>`
-   );
-     
-        for (let i=query.currentpage*query.blockpage; i<=query.currentpage * query.blockpage + NUMBERPAGE ; i++) {
-        if (i<= NUMBERPAGE*query.totalpages ) {
-          document.querySelector("#nextPage").insertAdjacentHTML("beforebegin", `<li class="waves-effect"><a href="#!">${i}</a></li>`);
-       }
-}
+} // funcion uirefresh
+
+
+function addPagination(){
+  //paginacion
+
+
+
+  
+  query.pagination = true;
+  paginacion.insertAdjacentHTML("beforeend",      
+`<li id="previusPage" class="wave-effects"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+<li class="active pageLink"><a href="#!">1</a></li>
+<li id="nextPage" class="wave-effects"><a href="#!"><i class="material-icons">chevron_right</i></a></li>`
+);
+
+  for (let i=2; i<=query.totalpages ; i++) {
+  if (i<= NUMBERPAGE) {
+    document.querySelector("#nextPage").insertAdjacentHTML("beforebegin", `<li class="waves-effect pageLink"><a href="#!">${i}</a></li>`);
+ } else {
+  document.querySelector("#nextPage").insertAdjacentHTML("beforebegin", `<li class="waves-effect hiddenpage pageLink"><a href="#!">${i}</a></li>`);
+
  }
+}
+}
 
-  if (query.currentpage < query.totalpages/NUMBERPAGE ){
-  document.getElementById("nextPage").classList.remove("disabled");
-  document.getElementById("nextPage").classList.add("waves-effect");
- };
-
-  } // funcion uirefresh
 
 paginacion.addEventListener("click", (event)=>{
   let target = event.target;
   if (target.tagName == "A") {
-    query.page = target.textContent;
     paginacion.querySelector(".active").classList.remove("active");
     target.closest('li').classList.add("active");
-    if (query.page == 1) {
-      document.getElementById("previusPage").classList.add("disabled");
-      document.getElementById("previusPage").classList.remove("waves-effect");
-    } else {
-      document.getElementById("previusPage").classList.remove("disabled");
-      document.getElementById("previusPage").classList.add("waves-effect");
-    }
+    query.page = target.textContent;
     getData();
   }else {
   if (target.tagName == "I" && !target.closest("li").classList.contains("disabled")) {
-    if (target.textContent == "chevron_left") {
-      query.page = query.page-1;
-      let currentPage = document.querySelector(".active");
-      currentPage.classList.remove("active");
-      currentPage.previousElementSibling.classList.add("active");
-      if (query.page == 1) {
-        document.getElementById("previusPage").classList.add("disabled");
-        document.getElementById("previusPage").classList.remove("waves-effect");
-      } else {
-        document.getElementById("previusPage").classList.remove("disabled");
-        document.getElementById("previusPage").classList.add("waves-effect");
-      }
-      getData();
-
+    if (target.textContent == "chevron_left" && query.blockpage>1) query.blockpage--;
+    if (target.textContent == "chevron_right" && query.blockpage<Math.ceil(query.totalpages/NUMBERPAGE)) query.blockpage++;
+    updatePagination();
+     {
+        getData();
     }
   }
   }
-
 }
-)
-    
+) ;
+
+function updatePagination(){
+  let maxRange = NUMBERPAGE * query.blockpage;
+  let minRange = 1+ NUMBERPAGE * (query.blockpage-1);
+  for (let link of paginacionLinks) {
+    if(link.children[0].textContent >= minRange && link.children[0].textContent <= maxRange) {
+      link.classList.remove("hiddenpage");
+    } else {
+      link.classList.add("hiddenpage");
+    }
+  }
+}
+
+
+//arrastrar y soltar
+
+cardsView.addEventListener("mousedown",(event)=>{
+  let target = event.target;
+  if(target.classList.contains("moveicon")){
+    target.closest(".draggable").style.display = hidden;
+
+  }
+
+
+})
+
+
+/* 
+let btn = document.querySelector('.add');
+let remove = document.querySelector('.draggable');
+
+function dragStart(e) {
+  this.style.opacity = '0.4';
+  dragSrcEl = this;
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+};
+
+function dragEnter(e) {
+  this.classList.add('over');
+}
+
+function dragLeave(e) {
+  e.stopPropagation();
+  this.classList.remove('over');
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
+
+function dragDrop(e) {
+  if (dragSrcEl != this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+  return false;
+}
+
+function dragEnd(e) {
+  var listItens = document.querySelectorAll('.draggable');
+  [].forEach.call(listItens, function(item) {
+    item.classList.remove('over');
+  });
+  this.style.opacity = '1';
+}
+
+function addEventsDragAndDrop(el) {
+  el.addEventListener('dragstart', dragStart, false);
+  el.addEventListener('dragenter', dragEnter, false);
+  el.addEventListener('dragover', dragOver, false);
+  el.addEventListener('dragleave', dragLeave, false);
+  el.addEventListener('drop', dragDrop, false);
+  el.addEventListener('dragend', dragEnd, false);
+}
+
+var listItens = document.querySelectorAll('.draggable');
+[].forEach.call(listItens, function(item) {
+  addEventsDragAndDrop(item);
+});
+
+function addNewItem() {
+  var newItem = document.querySelector('.input').value;
+  if (newItem != '') {
+    document.querySelector('.input').value = '';
+    var li = document.createElement('li');
+    var attr = document.createAttribute('draggable');
+    var ul = document.querySelector('ul');
+    li.className = 'draggable';
+    attr.value = 'true';
+    li.setAttributeNode(attr);
+    li.appendChild(document.createTextNode(newItem));
+    ul.appendChild(li);
+    addEventsDragAndDrop(li);
+  }
+}
+
+btn.addEventListener('click', addNewItem);
+ */
